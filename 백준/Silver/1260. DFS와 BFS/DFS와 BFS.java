34b3792dee76199777424326d15052f8bfa79850
BOMIN LYU <83059096+rbm0524@users.noreply.github.com>
2025-09-15 17:40:27 +0900

    [Silver II] Title: DFS와 BFS, Time: 168 ms, Memory: 17972 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,90 +1,73 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+public class Main{
 
-  static int N, M, V;
-  // static int[][] adj;// 인접 행렬로
-  static ArrayList<Integer>[] adj; // ArrayList를 원소로 가지는 배열!
-  static boolean[] visit; // default는 false
   static StringBuilder sb = new StringBuilder();
+  static int N, M, V;
+  static HashMap<Integer, ArrayList<Integer>> adj;
+  static boolean[] visited;
 
   static void dfs(int x) {
-    visit[x] = true; // x를 방문한 상태니까
-    sb.append(x).append(" "); // 처리
-    for (int y : adj[x]) { // iterator!
-      // if (adj[x][y] == 0) 0인 경우는 없을 것이니까 조건 검사 필요가 없음
-      // continue;
-      if (visit[y])
-        continue;
+    visited[x] = true;
+    sb.append(x).append(" ");
 
-      dfs(y);
-
+    for(int i : adj.get(x)) {
+      if(visited[i]) continue;
+      dfs(i);
     }
-
   }
 
-  static void bfs(int start) {
-    Queue<Integer> queue = new LinkedList<>();
+  static void bfs(int x) {
+    Queue<Integer> q = new LinkedList<>();
 
-    queue.add(start);
-    visit[start] = true;
+    q.add(x);
+    visited[x] = true;
+    while(!q.isEmpty()) {
+      int cur = q.poll();
+      sb.append(cur).append(" ");
 
-    while (!queue.isEmpty()) {
-      int x = queue.poll(); // x를 방문한 상태
-      sb.append(x).append(" ");
+      for(int i : adj.get(cur)) {
+        if(visited[i]) continue;
 
-      for (int y : adj[x]) {
-        if (visit[y])
-          continue;
-        queue.add(y);
-        visit[y] = true;
-
+        visited[i] = true;
+        q.offer(i);
       }
     }
-
   }
+  
+  public static void main(String args[]) throws IOException {
 
-  static void pro() {
-    visit = new boolean[N + 1];
-    dfs(V);
-    sb.append("\n");
-    for (int i = 1; i <= N; i++)
-      visit[i] = false;
-    bfs(V);
-
-  }
-
-  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-    st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
     V = Integer.parseInt(st.nextToken());
-    // adj = new int[N + 1][N + 1];
-    adj = new ArrayList[N + 1];
-
-    for (int i = 1; i <= N; i++) {
-      adj[i] = new ArrayList<Integer>();
+    adj = new HashMap<>();
+    for(int i = 1; i <= N; i++) {
+      adj.put(i, new ArrayList<>());
     }
-
-    for (int i = 1; i <= M; i++) {
+    
+    for(int i = 1; i <= M; i++) {
       st = new StringTokenizer(br.readLine());
       int v1 = Integer.parseInt(st.nextToken());
       int v2 = Integer.parseInt(st.nextToken());
-      adj[v1].add(v2);
-      adj[v2].add(v1);
+      adj.get(v1).add(v2);
+      adj.get(v2).add(v1);
     }
 
-    for (int i = 1; i <= N; i++) {
-      Collections.sort(adj[i]); // ArrayList는 Collections에서 sort
+    for(int i = 1; i <= N; i++) {
+      Collections.sort(adj.get(i));
     }
 
-    pro();
-
-    System.out.println(sb);
-
+    visited = new boolean[N+1];
+    dfs(V);
+    sb.append("\n");
+    visited = new boolean[N+1];
+    bfs(V);
+    System.out.println(sb);    
+    
   }
 }
+ 
